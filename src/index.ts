25c5e96167945b238ccbb1b64774c9283507f16b
Edixon Piña <edixonalbertto@gmail.com>
2020-12-12 00:34:40 -0400

    changes in InstagramApi, types and test
    - refactor class InstagramApi
    - modifying types
    - modifying test
    
    change small in readme

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { loadConfig } from './utils/loadConfig';
-import { TProfile, TLastPosts, TMedia } from './types';
+import type { TProfile, TLastPosts, TMedia, TPost } from './types';
 
 loadConfig();
 
-export { TProfile, TLastPosts, TMedia };
+export { TProfile, TLastPosts, TMedia, TPost };
 export { InstagramApi as instagrapi } from './InstagramApi';
