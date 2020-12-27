ee46830a93a78c5e0ec6aad3692df370bd52e011
Edixon Piña <edixonalbertto@gmail.com>
2020-12-26 21:46:37 -0400

    refactor and adding new test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,12 +1,13 @@
-import { Time } from '../src/utils/Time';
-import { instagrapi, TProfile, TLastPosts } from '../src/index';
+import { Utils } from '../src/utils';
+import { instagrapi, TProfile, TLastPosts, TPost, TComment } from '../src/index';
 
 describe('Utils', () => {
   test('Convert ms to date', () => {
-    const ms = 1607365446;
-    const date = Time.msToDate(ms);
+    const nowDate = new Date();
+    const ms = nowDate.getTime();
+    const date = Utils.msToDate(ms);
 
-    expect(date).toBe('2020-12-07T18:24:06.000Z');
+    expect(date).toBe(nowDate.toISOString());
   });
 });
 
@@ -19,5 +20,19 @@ describe('Instagrapi', () => {
   test('Get Last Posts', async () => {
     const lastPosts: TLastPosts = await instagrapi.getLastPosts('instagram');
     expect(lastPosts).toBeTruthy();
+  });
+
+  test('Get post', async () => {
+    const post: TPost = await instagrapi.getPost(
+      'https://www.instagram.com/p/CI8nNX0DC4U'
+    );
+
+    const responses = post.lastComments.filter(
+      (comment: TComment) => comment.responses.length > 0
+    );
+
+    expect(post.video!.duration).toBe(150.153);
+    expect(post.author.name).toBe('Instagram');
+    expect(responses[0]).toMatchObject(<TComment>{ author: { username: 'txqrx' } });
   });
 });
