d7fc3ca17002ecbc18bcae353858f271e3c0f4ae
Edixon Piña <edixonalbertto@gmail.com>
2021-01-04 01:20:46 -0400

    WIP: adding font and more styles
    - correnting filter
    - eddint script to deploy
    - adding links in main

--- demo/js/app.js
+++ demo/js/app.js
@@ -32,7 +32,9 @@ const vue = new Vue({
 // FILTERS
 
 Vue.filter('round', nro => {
-  return nro / 1000 < 1 ? nro : (nro / 1000).toFixed(0) + 'K';
+  if (nro / 1e6 < 1) {
+    return nro / 1e3 < 1 ? nro : (nro / 1e3).toFixed(0) + 'K';
+  } else return (nro / 1e6).toFixed(0) + 'M';
 });
 
 Vue.filter('cut', str => {
