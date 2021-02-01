5198871fd66608ac277489e66c05505d6ef30d86
Edixon Piña <edixonalbertto@gmail.com>
2021-02-01 17:53:20 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -6,25 +6,77 @@ const vue = new Vue({
   },
 
   data: {
+    features: {
+      col1: [
+        'Data based on the official instagram api',
+        'Data easy and fast to use',
+        'Data exposed in full JSON format',
+        'Images returned in standard and HD format',
+        'Dates returned in unirversal ISO format'
+      ],
+      col2: [
+        'Developed in NodeJS and Typescript',
+        'All types of data are exposed',
+        'Methods based in promises',
+        'Bundle optimized for use on the client',
+        'Code open source published in Github'
+      ]
+    },
+    username: '',
+    urlPost: '',
+    profile: null,
     lastPosts: [],
-    username: 'instagram',
-    profile: null
+    post: null
   },
 
-  created() {
-    if (this.username) {
-      this.searchProfile();
+  computed: {
+    cache: {
+      get() {
+        const data = JSON.parse(localStorage.getItem('data'));
+        return data || { profile: null, posts: [] };
+      },
+      set(newData) {
+        localStorage.clear();
+        localStorage.setItem('data', JSON.stringify(newData));
+      }
     }
   },
 
+  created() {
+    this.username = this.cache.profile?.username || 'instagram';
+    this.loadData(false);
+  },
+
   methods: {
-    async getPosts() {
-      this.lastPosts = await instagrapi.getLastPosts(this.username);
+    async loadData(cacheClear = true) {
+      if (cacheClear) this.cache = { profile: null, posts: [] };
+      console.log(this.cache);
+
+      await this.searchProfile();
+      await this.getPosts();
+
+      this.cache = {
+        profile: this.profile,
+        posts: this.lastPosts
+      };
     },
 
     async searchProfile() {
-      this.profile = await instagrapi.getProfile(this.username);
-      this.getPosts();
+      const profile = this.cache.profile;
+
+      this.profile = !profile ? await instagrapi.getProfile(this.username) : profile;
+    },
+
+    async getPosts() {
+      const posts = this.cache.posts;
+
+      this.lastPosts = !posts.length
+        ? await instagrapi.getLastPosts(this.username)
+        : posts;
+    },
+
+    async getPost() {
+      this.post = await instagrapi.getPost(urlPost);
     }
   }
 });
