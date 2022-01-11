36aa1ba53e0652cef820b7309efb1988d3e52068
EdixonAlberto <edixonalbertto@gmail.com>
2022-01-11 18:44:47 -0400

    implement minify in file createBundle, refactor scripts in package
    - refactor general in /tools

--- tools/createBundle.js
+++ tools/createBundle.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const { resolve } = require('path')
 const browserify = require('browserify')
+const UglifyJS = require('uglify-js')
+
 const { PATH_BASE, getHeaderMessage } = require('.')
 
 let browserifyObject = browserify(resolve('tools', 'templateBundle.js'), {
@@ -10,14 +12,32 @@ let browserifyObject = browserify(resolve('tools', 'templateBundle.js'), {
 browserifyObject.bundle((err, buffer) => {
   if (err) console.error('ERROR-BUNDLE ->', err.message)
   else {
-    const code = `${getHeaderMessage()}\n${buffer}`
+    const code = `${getHeaderMessage}\n${buffer}`
 
     fs.mkdir(PATH_BASE, null, err => {
       if (err) console.error('ERROR-BUNDLE ->', err.message)
       else {
-        fs.appendFile(resolve(PATH_BASE, 'instagrapi.js'), code, { encoding: 'utf8' }, err => {
+        const pathBundle = resolve(PATH_BASE, 'instagrapi.js')
+
+        fs.appendFile(pathBundle, code, { encoding: 'utf8' }, err => {
           if (err) console.error('ERROR-BUNDLE ->', err.message)
-          else console.log(`Bundle created in ${PATH_BASE}`)
+          else {
+            const code = fs.readFileSync(pathBundle, 'utf8')
+            fs.rm(pathBundle, () => null)
+
+            const result = UglifyJS.minify(code, {
+              output: {
+                preamble: getHeaderMessage()
+              },
+              warnings: true
+            })
+
+            if (result.code) {
+              fs.writeFile(resolve(PATH_BASE, 'instagrapi.min.js'), result.code, { encoding: 'utf8' }, () => {
+                console.log(`Bundle created in ${PATH_BASE}`)
+              })
+            } else console.error('ERROR-MINIFY ->', result.error.message || result.warnings)
+          }
         })
       }
     })
--- tools/index.js
+++ tools/index.js
@@ -1,16 +1,14 @@
 const fs = require('fs')
 const { resolve } = require('path')
 
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+const year = new Date().getFullYear().toString()
+
 module.exports.PATH_BASE = resolve('dist', 'bundle')
 
-module.exports.getHeaderMessage = () => {
-  const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-  const year = new Date().getFullYear().toString()
-
-  return `/*!
+module.exports.getHeaderMessage = `/*!
  *   ${pkg.name} v${pkg.version}
  *   ${pkg.description}
  *   Copyright (c) 2020-${year} ${pkg.author.name}
  *   Released under the ${pkg.license} License.
  */`
-}
