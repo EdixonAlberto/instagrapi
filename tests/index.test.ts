647b2e598b49fe5cf5b16beebfa7fc7e35e0ac1a
EdixonAlberto <edixonalbertto@gmail.com>
2022-01-13 21:54:19 -0400

    refactor tests, new test to the errors

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -13,26 +13,47 @@ describe('Utils', () => {
 })
 
 describe('Instagrapi', () => {
+  process.env.NODE_ENV = 'testing'
   config()
 
   const instagrapi = new Instagrapi({
     sessionId: process.env.SESSION_ID as string
   })
 
-  test('Get Profile', async () => {
-    const profile: TProfile = await instagrapi.getProfile('instagram')
-    expect(profile).toMatchObject(<TProfile>{ name: 'Instagram' })
+  describe('OK', () => {
+    test('Get profile', async () => {
+      const profile: TProfile = await instagrapi.getProfile('instagram')
+      expect(profile).toMatchObject(<TProfile>{ name: 'Instagram' })
+    })
+
+    test('Get last posts', async () => {
+      const lastPosts: TLastPosts = await instagrapi.getLastPosts('instagram')
+      expect(lastPosts).toBeTruthy()
+    })
+
+    test('Get post', async () => {
+      const post: TPost = await instagrapi.getPost('https://www.instagram.com/p/CI8nNX0DC4U')
+      const comment: TComment = post.lastComments[0]
+
+      expect(comment.author).toBeDefined()
+    })
   })
 
-  test('Get Last Posts', async () => {
-    const lastPosts: TLastPosts = await instagrapi.getLastPosts('instagram')
-    expect(lastPosts).toBeTruthy()
-  })
+  describe('ERROR', () => {
+    test('Username not found', async () => {
+      try {
+        await instagrapi.getProfile('abc')
+      } catch (error) {
+        expect(error).toMatchObject(<Error>{ message: 'Username not found' })
+      }
+    })
 
-  test('Get post', async () => {
-    const post: TPost = await instagrapi.getPost('https://www.instagram.com/p/CI8nNX0DC4U')
-    const comment: TComment = post.lastComments[0]
-
-    expect(comment.author).toBeDefined()
+    test('Post url not found', async () => {
+      try {
+        await instagrapi.getPost('http://urlfake')
+      } catch (error) {
+        expect(error).toMatchObject(<Error>{ message: 'Post url not found' })
+      }
+    })
   })
 })
